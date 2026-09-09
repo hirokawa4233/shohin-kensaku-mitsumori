c3c33d463dcae111f7928ab245e2387b112b08d6
hirokawa4233 <mmorita@hirokawasharyo.co.jp>
2026-09-09 14:49:51 +0900

    Implement CORS middleware in server.js
    Added CORS middleware to handle preflight requests.

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const crypto = require("crypto");
 const { Pool } = require("pg");
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 const PORT =
   process.env.PORT || 3000;
 
